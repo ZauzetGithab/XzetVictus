5ef176580c406942e2a0aad74e955ea0d752c6bc
ZauzetGithab <apk010947@gmail.com>
2025-07-19 11:46:40 +0700

    Create getUsers.js

---
+++ api/getUsers.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+  const REPO_OWNER = 'USERNAME_GITHUB_LO';
+  const REPO_NAME = 'REPO_NAMA_LO';
+  const FILE_PATH = 'users.json';
+
+  try {
+    const r = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`, {
+      headers: { Authorization: `token ${GITHUB_TOKEN}`, Accept: 'application/vnd.github.v3.raw' }
+    });
+    if (!r.ok) return res.status(500).json({ error: 'Gagal ambil data' });
+    const users = await r.json();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+      }
