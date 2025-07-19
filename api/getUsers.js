a525a92339958ebf0816a43ec650978c84bedba6
ZauzetGithab <apk010947@gmail.com>
2025-07-19 18:27:27 +0700

    Update getUsers.js

--- api/getUsers.js
+++ api/getUsers.js
@@ -1,15 +1,18 @@
 export default async function handler(req, res) {
-  const { GITHUB_TOKEN, REPO_OWNER, REPO_NAME } = process.env;
-  const FILE = "users.json";
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;  // masukin di Vercel Environment
+  const REPO_OWNER = 'ZauzetGithab';
+  const REPO_NAME = 'XzetVictus';
+  const FILE_PATH = 'users.json';
 
   try {
-    const r = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE}`, {
+    const r = await fetch(`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/contents/${FILE_PATH}`, {
       headers: { Authorization: `token ${GITHUB_TOKEN}` }
     });
+    if (!r.ok) return res.status(500).json({ error: 'Gagal ambil data' });
     const data = await r.json();
-    const content = Buffer.from(data.content, "base64").toString("utf8");
+    const content = Buffer.from(data.content, 'base64').toString('utf-8');
     res.status(200).json(JSON.parse(content));
   } catch (err) {
-    res.status(500).json({ error: "Gagal ambil data user" });
+    res.status(500).json({ error: err.message });
   }
 }
